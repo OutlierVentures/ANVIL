088048c905ae853981592a5b222c533c630790cc
Theo Turner <theo_turner@outlook.com>
2019-01-25 16:10:34 +0000

    Update startnode.sh for new install.sh

--- startnode.sh
+++ startnode.sh
@@ -5,5 +5,5 @@ onyellow='\033[43m'
 endcolor="\033[0m"
 
 echo -e "${onyellow}Starting node...$endcolor"
-./oef-core/oef-core-image/scripts/docker-run.sh -p 3333:3333 -d -- && \
+./oefcore/oef-core-image/scripts/docker-run.sh -p 3333:3333 -d -- && \
 echo -e "${ongreen}Node is running.$endcolor"
