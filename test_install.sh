201c51d0ff0314f2b1a4a92c8cb42a48f48027b1
Theo Turner <theo_turner@outlook.com>
2019-01-29 18:40:20 +0000

    Add install testing script

---
+++ test_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ongreen='\033[42m'
+onyellow='\033[43m'
+endcolor="\033[0m"
+
+echo -e "${onyellow}Testing Fetch install...$endcolor"
+cd oefpy
+sudo tox
+cd ..
+
+echo -e "${onyellow}Testing Sovrin install...$endcolor"
+# If tests run immediately after installer and node pool starter in the same session,
+# need to grab the dynamic linking envrionment variables
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    source ~/.bash_profile
+fi
+cd indy/wrappers/python
+pytest
+
+echo -e "${ongreen}Tests complete, see results above.$endcolor"
