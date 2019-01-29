225e96fa501b6c4236202c13a9827608b44a1885
Theo Turner <theo_turner@outlook.com>
2019-01-29 17:12:27 +0000

    Add Sovrin pool stopper script

---
+++ stop_sovrin_pool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+onred='\033[41m'
+onyellow='\033[43m'
+endcolor="\033[0m"
+
+echo -e "${onyellow}Stopping node...$endcolor"
+docker stop $(docker ps | grep indy_pool | awk '{ print $1 }')
+echo -e "${onred}Node stopped.$endcolor"
