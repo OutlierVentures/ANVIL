ce4200c32a99864b1e4df8bb7d4fe2d8b57f19ff
Theo Turner <theo_turner@outlook.com>
2019-01-29 17:10:21 +0000

    Fix fetch node stopping hang issue

--- stopnode.sh
+++ stopnode.sh
@@ -5,5 +5,5 @@ onyellow='\033[43m'
 endcolor="\033[0m"
 
 echo -e "${onyellow}Stopping node...$endcolor"
-docker stop $(docker ps | grep oef-core-image | awk '{ print $1 }') && \
+docker stop $(docker ps | grep oef-core-image | awk '{ print $1 }')
 echo -e "${onred}Node stopped.$endcolor"
