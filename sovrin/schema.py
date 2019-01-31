5581589af44182baa2f960ced95397bf81399073
Theo Turner <theo_turner@outlook.com>
2019-01-31 17:57:39 +0000

    Add credential schema file

---
+++ sovrin/schema.py
@@ -0,0 +1,9 @@
+'''
+Sovrin credential schema definitions.
+'''
+
+degree = {
+    'name': 'Certificate',
+    'version': '1.2',
+    'attributes': ['first_name', 'last_name', 'degree', 'status', 'year', 'average', 'ssn']
+}
