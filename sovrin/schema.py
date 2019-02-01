5a244ade2f1b9679d6c5630176259c3d0e5cbfcd
Theo Turner <theo_turner@outlook.com>
2019-02-01 13:16:54 +0000

    Update degree schema

--- sovrin/schema.py
+++ sovrin/schema.py
@@ -1,9 +1,11 @@
 '''
 Sovrin credential schema definitions.
+
+Schema must have a name field.
 '''
 
 degree = {
-    'name': 'Certificate',
+    'name': 'Degree Certificate',
     'version': '1.2',
     'attributes': ['first_name', 'last_name', 'degree', 'status', 'year', 'average', 'ssn']
 }
