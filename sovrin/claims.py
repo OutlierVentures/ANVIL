86224cad27fbe2848caa902b7c06c8396b2bd205
Theo Turner <theo_turner@outlook.com>
2019-02-12 15:19:46 +0000

    Claims up to credential issuance and storage individualised

--- sovrin/claims.py
+++ sovrin/claims.py
@@ -5,10 +5,19 @@ from ctypes import CDLL
 from sovrin_utilities import run_coroutine, send_data, receive_data
 
 from setup import setup_pool, setup_steward, teardown
-from onboarding import simple_onboard, onboard_for_proving, onboarding
+from onboarding import simple_onboard, onboard_for_proving, onboarding, new_onboard, new_pairwise_onboard
 from credentials import create_schema, create_credential_definition
 from issue import offer_credential, receive_credential_offer, request_credential, create_and_send_credential, store_credential
 from proofs import request_proof_of_credential, create_proof_of_credential, verify_proof
+
+
+'''
+It is recommended to keep the actor names as steward, issuer, prover, & verifier (all lowercase).
+User-facing names are implemented on the Fetch side.
+Changing names means going through the modules (esp. issue.py) and dynamically naming fields
+[actor]_key or [actor]_did depending on the context.
+'''
+
 
 
 logger = logging.getLogger(__name__)
@@ -56,11 +65,14 @@ async def run():
     pool_name, pool_handle = await setup_pool('ANVIL')
 
     steward = await setup_steward(pool_handle = pool_handle,
-                                  name = 'Steward',
+                                  name = 'steward',
                                   id_ = 'mocked_steward_id',
                                   key = 'mocked_steward_key',
                                   seed = '000000000000000000000000Steward1')
-                                  
+
+    steward, issuer = await new_onboard(steward, 'issuer', 'mocked_issuer_id', 'mocked_issuer_key', pool_handle)                           
+    issuer, prover = await new_onboard(issuer, 'prover', 'mocked_prover_id', 'mocked_prover_key', pool_handle)  
+    '''
     issuer, steward = await simple_onboard(pool_handle = pool_handle,
                                            anchor = steward,
                                            name = 'issuer',
@@ -72,7 +84,7 @@ async def run():
                                                name = 'prover',
                                                id_ = 'mocked_prover_id',
                                                key = 'mocked_prover_key')
-
+    '''
     verifier, steward = await simple_onboard(pool_handle = pool_handle,
                                              anchor = steward,
                                              name = 'verifier',
