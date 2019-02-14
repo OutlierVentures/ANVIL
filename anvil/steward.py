a5643192909cb4d7d31fa1c3ecbc4964558c29cb
Theo Turner <theo_turner@outlook.com>
2019-02-14 09:50:07 +0000

    Quart: dynamic display functions, full setup

--- anvil/steward.py
+++ anvil/steward.py
@@ -2,6 +2,7 @@ import os, asyncio
 from quart import Quart, render_template, redirect, url_for, session
 from sovrin.utilities import generate_base58
 from sovrin.setup import setup_pool, set_self_up
+from sovrin.onboarding import onboarding_anchor_send
 #from sovrin.utilities import generate_base58
 app = Quart(__name__)
 
@@ -12,24 +13,29 @@ port = 5000
 
 @app.route('/')
 def index():
-    return render_template('anvil.html', actor = 'anchor')
+    steward = session.get('steward')
+    setup = True if steward else False
+    return render_template('anvil.html', actor = 'steward', setup = setup)
 
 
 @app.route('/setup', methods = ['GET', 'POST'])
-async def setupl():
+async def setup():
     session['pool_name'], session['pool_handle'] = await setup_pool('ANVIL')
     pool_handle = session.get('pool_handle')
-    print(pool_handle)
+    id_ = os.getenv('ANVIL_ID', generate_base58(64))
+    key = os.getenv('ANVIL_KEY', generate_base58(64))
+    seed = os.getenv('ANVIL_SEED', '000000000000000000000000Steward1')
+    session['steward'] = await set_self_up('steward', id_, key, session['pool_handle'], seed = seed)
+    print(pool_handle, session['steward'])
     return redirect(url_for('index'))
 
 
 @app.route('/connection_request', methods = ['GET', 'POST'])
 async def connection_request():
-    id_ = os.getenv('ANVIL_ID', generate_base58(64))
-    key = os.getenv('ANVIL_KEY', generate_base58(64))
-    seed = os.getenv('ANVIL_SEED', '000000000000000000000000Steward1')
-    session['steward'] = await set_self_up('steward', id_, key, session['pool_handle'], seed = seed)
     steward = session.get('steward')
+    unique_onboardee_name = 'abc' # get from input
+    unique_onboardee_name = ''.join(e for e in unique_onboardee_name if e.isalnum())
+    steward, connection_request = onboarding_anchor_send(steward, unique_onboardee_name)
     print(steward)
     return redirect(url_for('index'))
 
