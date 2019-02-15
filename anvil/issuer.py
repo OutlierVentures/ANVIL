81ae513a7ac7b0874182896ca4ed7e802b47a43e
Theo Turner <theo_turner@outlook.com>
2019-02-15 17:27:52 +0000

    Async issuer

--- anvil/issuer.py
+++ anvil/issuer.py
@@ -1,8 +1,8 @@
-import os, requests, json
+import os, requests, json, time
 from quart import Quart, render_template, redirect, url_for, session, request, jsonify
-from sovrin.utilities import generate_base58
+from sovrin.utilities import generate_base58, run_coroutine
 from sovrin.setup import setup_pool, set_self_up, teardown
-from sovrin.onboarding import onboarding_anchor_send
+from sovrin.onboarding import onboarding_anchor_send, onboarding_onboardee_receive_and_send
 app = Quart(__name__)
 
 debug = True # Do not enable in production
@@ -24,15 +24,28 @@ async def setup():
     id_ = os.getenv('WALLET_ID', generate_base58(64))
     key = os.getenv('WALLET_KEY', generate_base58(64))
     session['issuer'] = await set_self_up('issuer', id_, key, session['pool_handle'])
-    print(pool_handle, session['issuer'])
+    print(type(session['issuer']))
     return redirect(url_for('index'))
 
 
 @app.route('/data', methods = ['GET', 'POST'])
 async def data():
     data = await request.data
-    print(data)
-    return data
+    issuer = session.get('issuer')
+    pool_handle = session.get('pool_handle')
+    while not issuer:
+        issuer = session.get('issuer')
+        time.sleep(0.1)
+    else:
+        session['issuer'], anoncrypted_connection_response = await onboarding_onboardee_receive_and_send(issuer, data, pool_handle, 'steward')
+    while not anoncrypted_connection_response:
+        time.sleep(0.1)
+    else:
+        requests.post('http://' + request.remote_addr + ':' + request.remote_port + '/data', json = anoncrypted_connection_response)
+    return 'yup'#data
+
+
+
 
 @app.route('/reset')
 def reset():
