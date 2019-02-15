aaa448e22db5f75e10cdc271ef81117c45ed9120
Theo Turner <theo_turner@outlook.com>
2019-02-15 20:17:22 +0000

    Issuer conditional reply

--- anvil/issuer.py
+++ anvil/issuer.py
@@ -7,50 +7,60 @@ app = Quart(__name__)
 
 debug = True # Do not enable in production
 host = '0.0.0.0'
+# In production everyone runs on same port, use 2 here for same-machine testing 
 port = 5001
+receiver_port = 5000 
+
+# Globals approach will be dropped once session persistence in Python is fixed.
+issuer = {}
+pool_handle = 1
+received_data = ''
 
 
 @app.route('/')
 def index():
-    issuer = session.get('issuer')
-    setup = True if issuer else False
-    return render_template('issuer.html', actor = 'issuer', setup = setup)
-
+    global issuer, received_data
+    setup = True if issuer != {} else False
+    have_data = True if received_data != '' else False
+    return render_template('issuer.html', actor = 'issuer', setup = setup, have_data = have_data)
+ 
 
 @app.route('/setup', methods = ['GET', 'POST'])
 async def setup():
-    session['pool_name'], session['pool_handle'] = await setup_pool('ANVIL')
-    pool_handle = session.get('pool_handle')
+    global issuer
+    _, pool_handle = await setup_pool('ANVIL')
     id_ = os.getenv('WALLET_ID', generate_base58(64))
     key = os.getenv('WALLET_KEY', generate_base58(64))
-    session['issuer'] = await set_self_up('issuer', id_, key, session['pool_handle'])
-    print(type(session['issuer']))
+    issuer = await set_self_up('issuer', id_, key, pool_handle)
     return redirect(url_for('index'))
 
 
-@app.route('/data', methods = ['GET', 'POST'])
+@app.route('/receive', methods = ['GET', 'POST'])
 async def data():
-    data = await request.data
-    issuer = session.get('issuer')
-    pool_handle = session.get('pool_handle')
-    while not issuer:
-        issuer = session.get('issuer')
-        time.sleep(0.1)
-    else:
-        session['issuer'], anoncrypted_connection_response = await onboarding_onboardee_receive_and_send(issuer, data, pool_handle, 'steward')
-    while not anoncrypted_connection_response:
-        time.sleep(0.1)
-    else:
-        requests.post('http://' + request.remote_addr + ':' + request.remote_port + '/data', json = anoncrypted_connection_response)
-    return 'yup'#data
+    global received_data
+    received_data = await request.data
+    print(received_data)
+    return '200'
 
-
+@app.route('/respond', methods = ['GET', 'POST'])
+async def respond():
+    global issuer, received_data
+    data = json.loads(received_data)
+    issuer, anoncrypted_connection_response = await onboarding_onboardee_receive_and_send(issuer, data, pool_handle, 'steward')
+    requests.post('http://' + request.remote_addr + ':' + str(receiver_port) + '/establish_channel', anoncrypted_connection_response)
+    return redirect(url_for('index'))
 
 
 @app.route('/reset')
 def reset():
-    #teardown(session.get('pool_name'), session.get('pool_handle'), [session.get('issuer')])
-    session.clear()
+    global issuer
+    teardown('ANVIL', pool_handle, [issuer])
+    issuer = {}
+    session.clear() # Possibly unnecessary
+    return redirect(url_for('index'))
+
+@app.route('/reload')
+def reload():
     return redirect(url_for('index'))
 
 
