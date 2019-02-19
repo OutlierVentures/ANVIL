ab703d3c718b191b412268a14ce3b08519f2a0b9
Theo Turner <theo_turner@outlook.com>
2019-02-19 15:44:14 +0000

    Prover full credential request and storage

--- anvil/prover.py
+++ anvil/prover.py
@@ -1,7 +1,7 @@
 import os, requests, json, time
 from quart import Quart, render_template, redirect, url_for, request
 from common import common_setup, common_respond, common_get_verinym, common_reset
-from sovrin.credentials import receive_credential_offer, request_credential
+from sovrin.credentials import receive_credential_offer, request_credential, store_credential
 app = Quart(__name__)
 
 debug = True # Do not enable in production
@@ -12,15 +12,15 @@ receiver_port = 5001
 
 # Globals approach will be dropped once session persistence in Python is fixed.
 prover = {}
-pool_handle = 1
 request_ip = anchor_ip = received_data = False
-created_schema = []
+pool_handle = 1
+stored_credentials = []
 
 
 @app.route('/')
 def index():
-    setup = True if prover != {} else False
-    have_data = True if received_data != False else False
+    setup = True if prover else False
+    have_data = True if received_data else False
     responded = True if 'connection_response' in prover else False
     '''
     The onboardee depends on the anchor to finish establishing the secure channel.
@@ -29,10 +29,10 @@ def index():
     relevant response from the anchor is returned, which is only possible if the channel
     is set up on the anchor end.
     '''
-    channel_established = True if anchor_ip != '' else False
+    channel_established = True if anchor_ip else False
     have_verinym = True if 'did_info' in prover else False
     unique_schema_name = prover['unique_schema_name'] if 'unique_schema_name' in prover else False
-    return render_template('prover.html', actor = 'prover', setup = setup, have_data = have_data, request_ip = request_ip, responded = responded, channel_established = channel_established, have_verinym = have_verinym, created_schema = created_schema, unique_schema_name = unique_schema_name)
+    return render_template('prover.html', actor = 'prover', setup = setup, have_data = have_data, request_ip = request_ip, responded = responded, channel_established = channel_established, have_verinym = have_verinym, stored_credentials = stored_credentials, unique_schema_name = unique_schema_name)
  
 
 @app.route('/setup', methods = ['GET', 'POST'])
@@ -44,8 +44,9 @@ async def setup():
 
 @app.route('/receive', methods = ['GET', 'POST'])
 async def data():
-    global received_data
+    global received_data, request_ip
     received_data = await request.data
+    request_ip = request.remote_addr
     print(received_data)
     return '200'
 
@@ -77,9 +78,22 @@ async def credential_inbox():
 async def request_credential_from_issuer():
     global prover
     form = await request.form
-    prover = await request_credential(prover, form['credential_request'])
-    requests.post('http://' + anchor_ip + ':' + str(receiver_port) + '/credential_inbox', prover['authcrypted_cred_request'])
+    credential_request = form['credrequest'] # Request credential demands a string-formatted JSON
+    prover = await request_credential(prover, credential_request)
+    requests.post('http://' + anchor_ip + ':' + str(receiver_port) + '/credential_request', prover['authcrypted_cred_request'])
     return redirect(url_for('index'))
+
+
+@app.route('/credential_store', methods = ['GET', 'POST'])
+async def credential_store():
+    global prover, stored_credentials
+    try:
+        prover['authcrypted_cred'] = await request.data
+        prover = await store_credential(prover)
+        stored_credentials.append(prover['unique_schema_name']) # May cause failure of block if schema exists but name hasnt been stored - just store name
+        return '200'
+    except:
+        return 'Invalid credential. Check you are authcrypting with the verification key for this actor.'
 
 
 @app.route('/reset')
