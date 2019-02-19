365f47a38ba8170992e709ba8f7af4d94251e1b6
Theo Turner <theo_turner@outlook.com>
2019-02-19 20:35:03 +0000

    Verifier: proof requests, proof inbox and verification function

--- anvil/verifier.py
+++ anvil/verifier.py
@@ -3,13 +3,14 @@ from quart import Quart, render_template, redirect, url_for, request
 from common import common_setup, common_respond, common_get_verinym, common_reset, common_connection_request, common_establish_channel, common_verinym_request
 from sovrin.schema import create_schema, create_credential_definition
 from sovrin.credentials import offer_credential, create_and_send_credential
+from sovrin.proofs import request_proof_of_credential, verify_proof
 app = Quart(__name__)
 
 debug = True # Do not enable in production
 host = '0.0.0.0'
 # In production everyone runs on same port, use 2 here for same-machine testing 
 port = 5003
-receiver_port = 5000
+anchor_port = 5000
 prover_port = 5002
 
 # Globals approach will be dropped once session persistence in Python is fixed.
@@ -32,10 +33,11 @@ def index():
     is set up on the anchor end.
     '''
     channel_established = True if anchor_ip else False
-    prover_registered = True if 'prover_registered' in verifier else False
+    prover_registered = True if 'prover_ip' in verifier else False
     have_verinym = True if 'did_info' in verifier else False
     credential_requested = True if 'authcrypted_cred_request' in verifier else False
-    return render_template('verifier.html', actor = 'verifier', setup = setup, have_data = have_data, request_ip = request_ip, responded = responded, channel_established = channel_established, have_verinym = have_verinym, created_schema = created_schema, prover_registered = prover_registered, credential_requested = credential_requested, counterparty_name = counterparty_name)
+    have_proof = True if 'authcrypted_proof' in verifier else False
+    return render_template('verifier.html', actor = 'verifier', setup = setup, have_data = have_data, request_ip = request_ip, responded = responded, channel_established = channel_established, have_verinym = have_verinym, created_schema = created_schema, prover_registered = prover_registered, credential_requested = credential_requested, have_proof = have_proof)
  
 
 @app.route('/setup', methods = ['GET', 'POST'])
@@ -57,14 +59,14 @@ async def data():
 @app.route('/respond', methods = ['GET', 'POST'])
 async def respond():
     global verifier, anchor_ip
-    verifier, anchor_ip = await common_respond(verifier, received_data, pool_handle, receiver_port)
+    verifier, anchor_ip = await common_respond(verifier, received_data, pool_handle, anchor_port)
     return redirect(url_for('index'))
 
 
 @app.route('/get_verinym', methods = ['GET', 'POST'])
 async def get_verinym():
     global verifier
-    verifier = await common_get_verinym(verifier, anchor_ip, receiver_port)
+    verifier = await common_get_verinym(verifier, anchor_ip, anchor_port)
     return redirect(url_for('index'))
 
 
@@ -80,7 +82,7 @@ async def establish_channel():
     global verifier
     verifier = await common_establish_channel(verifier, counterparty_name)
     # Connection with multiple actor types (e.g. steward + prover) demands registering this at the app level
-    verifier['prover_registered'] = 'yes'
+    verifier['prover_ip'] = request.remote_addr
     return '200'
 
 
@@ -97,19 +99,45 @@ async def request_proof():
     try:
         form = await request.form
         proof_request = json.loads(form['proofrequest'])
-        verifier = await request_proof_of_credential(verifier, proof_request)
-        requests.post('http://' + anchor_ip + ':' + str(prover_port) + '/proof_request', verifier['authcrypted_proof_request'])
+        '''
+        Proof requests have 2 parts:
+        1. Request: the requested attributes/predicates (to be sent to prover).
+        2. Assertions: the assertions about the attributes/predicates to ensure are true.
+        '''
+        request_json = json.dumps(proof_request['request'])
+        verifier['assertions_to_make'] = proof_request['assertions_to_make']
+        verifier = await request_proof_of_credential(verifier, request_json)
+        requests.post('http://' + verifier['prover_ip'] + ':' + str(prover_port) + '/proof_request', verifier['authcrypted_proof_request'])
         return redirect(url_for('index'))
     except:
-        return 'Invalid schema. Check formatting.'
+        return 'Invalid proof request. Check formatting.'
+
+
+@app.route('/proof_inbox', methods = ['GET', 'POST'])
+async def proof_inbox():
+    global verifier
+    verifier['authcrypted_proof'] = await request.data
+    return '200'
+
+
+@app.route('/verify', methods = ['GET', 'POST'])
+async def verify():
+    global verifier
+    try:
+        verifier = await verify_proof(verifier, verifier['assertions_to_make'])
+        # Hide verify function until next proof received
+        verifier.pop('authcrypted_proof', None)
+        return redirect(url_for('index'))
+    except:
+        return 'Proof invalid. Potentially check your own assertions on the values.'
 
 
 @app.route('/reset')
 def reset():
     global verifier, pool_handle, received_data, anchor_ip
     verifier, pool_handle = common_reset([verifier], pool_handle)
-    received_data = ''
-    anchor_ip = ''
+    received_data = False
+    anchor_ip = False
     return redirect(url_for('index'))
 
 
