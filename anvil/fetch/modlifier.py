8167d708eea4c5e569542f34d893014418999b98
Theo Turner <theo_turner@outlook.com>
2019-02-20 15:28:25 +0000

    Drop descriptionify, add json file loader to modlifier

--- anvil/fetch/modlifier.py
+++ anvil/fetch/modlifier.py
@@ -6,22 +6,20 @@ import json
 
 from oef.schema import AttributeSchema, DataModel, Description
 
-def modlify(path_to_data_model):
-    with open(path_to_data_model) as file_:
-        data = json.load(file_)
+def modlify(data):
     attributes = data['attributes']
     # This is the least restrictive apporach.
     # To make attributes required override the specific one with True as the third argument.
     attribute_list = []
     for key, value in attributes.items():
         attribute_list.append(AttributeSchema(key, bool, False, value))
-
     data_model = DataModel(data['name'], attribute_list, data['description'])
     return data_model
 
-def descriptionify(data_model, path_to_description):
-    with open(path_to_description) as file_:
+
+def load_json_file(path):
+    with open(path) as file_:
         data = json.load(file_)
-    description = Description(data, data_model)
-    return description
+    return data
+
 
