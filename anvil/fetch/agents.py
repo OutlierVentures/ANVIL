26f1e60517968b3b1a7389ad45f3b71fb822bbec
Theo Turner <theo_turner@outlook.com>
2019-03-19 16:33:46 +0000

    API: add fetch syntactic sugar wrappers

---
+++ anvil/fetch/agents.py
@@ -0,0 +1,22 @@
+'''
+Fetch AEA functions:
+
+1. Search the OEF.
+2. Offer a Fetch service.
+3. Purchase a Fetch service.
+'''
+
+
+import subprocess
+
+
+def search(search_terms, path_to_fetch_folder = './fetch'):
+    subprocess.run('python3 ' + path_to_fetch_folder + '/searcher.py ' + search_terms, shell = True)
+
+
+def offer_service(price, service_path, path_to_fetch_folder = './fetch'):
+    subprocess.Popen('python3 ' + path_to_fetch_folder + '/prover.py ' + service_path + ' ' + str(price), shell = True)
+
+
+def purchase_service(max_price, search_terms, path_to_fetch_folder = './fetch'):
+    subprocess.run('python3 ' + path_to_fetch_folder + '/verifier.py ' + search_terms + ' ' + str(max_price), shell = True)
