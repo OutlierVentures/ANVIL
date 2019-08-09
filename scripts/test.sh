65efb762f103212f91ec5644bd8a61c782888801
Theo Turner <theo_turner@outlook.com>
2019-08-09 11:35:41 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,8 @@ ongreen='\033[42m'
 onyellow='\033[43m'
 endcolor="\033[0m"
 
-echo -e "${onyellow}Testing Fetch install...$endcolor"
-cd oefpy
-sudo tox -e py37
-cd ..
+#echo -e "${onyellow}Testing Fetch install...$endcolor"
+
 
 echo -e "${onyellow}Testing Sovrin install...$endcolor"
 # If tests run immediately after installer and node pool starter in the same session,
